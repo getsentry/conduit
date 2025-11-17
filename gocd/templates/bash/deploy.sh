9eae13de01a830b10edcf4467400d2afe886d051
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2025-11-17 12:55:07 -0800

    fix(deploy): Fix the deploy script to use correct labels and names (#38)

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -4,10 +4,10 @@ eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 
 /devinfra/scripts/get-cluster-credentials \
 && k8s-deploy \
-  --label-selector="${LABEL_SELECTOR}" \
+  --label-selector="${LABEL_SELECTOR},component=gateway" \
   --image="us-central1-docker.pkg.dev/sentryio/conduit/conduit-gateway:${GO_REVISION_CONDUIT_REPO}" \
-  --container-name="conduit-gateway" \
+  --container-name="conduit" \
 && k8s-deploy \
-  --label-selector="${LABEL_SELECTOR}" \
+  --label-selector="${LABEL_SELECTOR},component=publish" \
   --image="us-central1-docker.pkg.dev/sentryio/conduit/conduit-publish:${GO_REVISION_CONDUIT_REPO}" \
-  --container-name="conduit-publish"
+  --container-name="conduit"
