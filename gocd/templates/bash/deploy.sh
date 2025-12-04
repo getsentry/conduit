f36eda034675aff8d2740f44a73e12ce179541f9
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2025-12-04 13:37:45 -0800

    ref(deploy): Switch to multi-region image for deploy (#54)

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -5,9 +5,9 @@ eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 /devinfra/scripts/get-cluster-credentials \
 && k8s-deploy \
   --label-selector="${LABEL_SELECTOR},component=gateway" \
-  --image="us-central1-docker.pkg.dev/sentryio/conduit/conduit-gateway:${GO_REVISION_CONDUIT_REPO}" \
+  --image="us-docker.pkg.dev/sentryio/conduit-mr/conduit-gateway:${GO_REVISION_CONDUIT_REPO}" \
   --container-name="conduit" \
 && k8s-deploy \
   --label-selector="${LABEL_SELECTOR},component=publish" \
-  --image="us-central1-docker.pkg.dev/sentryio/conduit/conduit-publish:${GO_REVISION_CONDUIT_REPO}" \
+  --image="us-docker.pkg.dev/sentryio/conduit-mr/conduit-publish:${GO_REVISION_CONDUIT_REPO}" \
   --container-name="conduit"
