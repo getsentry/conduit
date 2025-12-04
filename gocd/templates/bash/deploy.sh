22796b981461f100607fbbeee72712ec478b011a
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2025-12-04 15:06:57 -0800

    ref(cleanup): Add cleanup service to more places (#55)
    * ref(cleanup): Add cleanup service to more places
    
    * added in another place

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -10,4 +10,8 @@ eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 && k8s-deploy \
   --label-selector="${LABEL_SELECTOR},component=publish" \
   --image="us-docker.pkg.dev/sentryio/conduit-mr/conduit-publish:${GO_REVISION_CONDUIT_REPO}" \
+  --container-name="conduit" \
+&& k8s-deploy \
+  --label-selector="${LABEL_SELECTOR},component=cleanup" \
+  --image="us-docker.pkg.dev/sentryio/conduit-mr/conduit-cleanup:${GO_REVISION_CONDUIT_REPO}" \
   --container-name="conduit"
